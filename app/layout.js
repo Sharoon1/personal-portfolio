af80886d3b00da7912bb19f3f69007d56c002102
sharoon110 <sharoonsarfraz001@gmail.com>
2025-04-05 11:56:17 +0500

    Added Google site verification meta tag

--- app/layout.js
+++ app/layout.js
@@ -17,6 +17,10 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <head>
+        <meta
+          name="google-site-verification"
+          content="pTX8xC8PEKFiIV2O_rvlf2B1o5xQtFaGaZ5thns_e2c"
+        />
         <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
         {/* <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"/> */}
       </head>
